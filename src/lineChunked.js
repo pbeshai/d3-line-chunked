f5b4b7158a33d959ec117d95e26daac21c683b0e
Peter Beshai <peter.beshai@gmail.com>
2016-08-27 12:51:40 -0400

    Fix computeSegments to use properties and update documentation

--- src/lineChunked.js
+++ src/lineChunked.js
@@ -4,54 +4,6 @@ import { curveLinear, line as d3Line } from 'd3-shape';
 import { interpolatePath } from 'd3-interpolate-path'; // only needed if using transitions
 
 /**
- * Helper function to compute the contiguous segments of the data
- * @param {Array} lineData the line data
- * @return {Array} An array of segments (subarrays) of the line data
- */
-function computeSegments(lineData) {
-  let startNewSegment = true;
-
-  // we expect x to increment by this amount. if it does not,
-  // then data is missing.
-  function isExpectedNext(previousDatum, currentDatum) {
-    const expectedXIncrement = 1;
-    return (currentDatum.x === previousDatum.x + expectedXIncrement);
-  }
-
-  // split into segments of continuous data
-  const segments = lineData.reduce((segments, d) => {
-    // skip if this point has no data
-    if (d.y == null) {
-      startNewSegment = true;
-      return segments;
-    }
-
-    // if we are starting a new segment, start it with this point
-    if (startNewSegment) {
-      segments.push([d]);
-      startNewSegment = false;
-
-    // otherwise see if we are adding to the last segment
-    } else {
-      const lastSegment = segments[segments.length - 1];
-      const lastDatum = lastSegment[lastSegment.length - 1];
-      // if we expect this point to come next, add it to the segment
-      if (isExpectedNext(lastDatum, d)) {
-        lastSegment.push(d);
-
-      // otherwise create a new segment
-      } else {
-        segments.push([d]);
-      }
-    }
-
-    return segments;
-  }, []);
-
-  return segments;
-}
-
-/**
  * Renders line with potential gaps in the data by styling the gaps differently
  * from the defined areas. Single points are rendered as circles. Transitions are
  * supported.
@@ -68,23 +20,137 @@ export default function () {
     'stroke-opacity': 0.2,
   };
   const defaultPointAttrs = {
-    class: 'chunk-defined-point',
     // read fill and r at render time in case the lineAttrs changed
     // fill: defaultLineAttrs.stroke,
     // r: defaultLineAttrs['stroke-width'],
   };
 
+  /**
+   * How to access the x attribute of `d`
+   */
   let x = d => d[0];
+
+  /**
+   * How to access the y attribute of `d`
+   */
   let y = d => d[1];
+
+  /**
+   * Function to determine if there is data for a given point.
+   * @param {Any} d data point
+   * @return {Boolean} true if the data is defined for the point, false otherwise
+   */
   let defined = () => true;
+
+  /**
+   * Function to determine if there a point follows the previous. This functions
+   * enables detecting gaps in the data when there is an unexpected jump. For
+   * instance, if you have time data for every day and the previous data point
+   * is for January 5, 2016 and the current data point is for January 12, 2016,
+   * then there is data missing for January 6-11, so this function would return
+   * true.
+   *
+   * It is only necessary to define this if your data doesn't explicitly include
+   * gaps in it.
+   *
+   * @param {Any} previousDatum The previous data point
+   * @param {Any} currentDatum The data point under consideration
+   * @return {Boolean} true If the data is defined for the point, false otherwise
+   */
+  let isNext = () => true;
+
+  /**
+   * Passed through to d3.line().curve. Default value: d3.curveLinear.
+   */
   let curve = curveLinear;
+
+  /**
+   * Object mapping style keys to style values to be applied to both
+   * defined and undefined lines. Uses syntax similar to d3-selection-multi.
+   */
   let lineStyles = {};
+
+  /**
+   * Object mapping attr keys to attr values to be applied to both
+   * defined and undefined lines. Uses syntax similar to d3-selection-multi.
+   */
   let lineAttrs = defaultLineAttrs;
+
+  /**
+   * Object mapping style keys to style values to be applied only to the
+   * undefined lines. It overrides values provided in lineStyles. Uses
+   * syntax similar to d3-selection-multi.
+   */
   let gapStyles = {};
+
+  /**
+   * Object mapping attr keys to attr values to be applied only to the
+   * undefined lines. It overrides values provided in lineAttrs. Uses
+   * syntax similar to d3-selection-multi.
+   */
   let gapAttrs = defaultGapAttrs;
+
+  /**
+   * Object mapping style keys to style values to be applied to points.
+   * Uses syntax similar to d3-selection-multi.
+   */
   let pointStyles = {};
+
+  /**
+   * Object mapping attr keys to attr values to be applied to points.
+   * Note that if fill is not defined in pointStyles or pointAttrs, it
+   * will be read from the stroke color on the line itself.
+   * Uses syntax similar to d3-selection-multi.
+   */
   let pointAttrs = defaultPointAttrs;
+
+  /**
+   * Flag to set whether to transition on initial render or not. If true,
+   * the line starts out flat and transitions in its y value. If false,
+   * it just immediately renders.
+   */
   let transitionInitial = true;
+
+  /**
+   * Helper function to compute the contiguous segments of the data
+   * @param {Array} lineData the line data
+   * @return {Array} An array of segments (subarrays) of the line data
+   */
+  function computeSegments(lineData) {
+    let startNewSegment = true;
+
+    // split into segments of continuous data
+    const segments = lineData.reduce((segments, d) => {
+      // skip if this point has no data
+      if (!defined(d)) {
+        startNewSegment = true;
+        return segments;
+      }
+
+      // if we are starting a new segment, start it with this point
+      if (startNewSegment) {
+        segments.push([d]);
+        startNewSegment = false;
+
+      // otherwise see if we are adding to the last segment
+      } else {
+        const lastSegment = segments[segments.length - 1];
+        const lastDatum = lastSegment[lastSegment.length - 1];
+        // if we expect this point to come next, add it to the segment
+        if (isNext(lastDatum, d)) {
+          lastSegment.push(d);
+
+        // otherwise create a new segment
+        } else {
+          segments.push([d]);
+        }
+      }
+
+      return segments;
+    }, []);
+
+    return segments;
+  }
 
   /**
    * Render the points for when segments have length 1.
@@ -127,6 +193,7 @@ export default function () {
     });
 
     circlesEnter
+      .classed('d3-line-chunked-defined-point', true)
       .attr('r', 1e-6) // overrides provided `r value for now
       .attr('cx', d => x(d.data))
       .attr('cy', d => y(d.data));
@@ -273,10 +340,15 @@ export default function () {
     const segments = computeSegments(lineData);
     const points = segments.filter(segment => segment.length === 1)
       .map(segment => ({
-        id: Math.random(), // use random ID so they are treated as entering/exiting each time
+        // use random ID so they are treated as entering/exiting each time
+        id: Math.random(),
         data: segment[0],
       }));
-    const filteredLineData = lineData.filter(d => y(d) != null && !isNaN(y(d)));
+
+    // filter to only defined data to plot the lines
+    const filteredLineData = lineData.filter(defined);
+
+    // determine the extent of the y values
     const yExtent = extent(filteredLineData.map(d => y(d)));
 
     const initialRender = selection.select('.d3-line-chunked-defined').empty();
@@ -325,6 +397,14 @@ export default function () {
   lineChunked.defined = getterSetter({
     get: () => defined,
     set: (newValue) => { defined = newValue; },
+    setType: 'function',
+    asConstant: (newValue) => () => !!newValue,
+  });
+
+  // define `isNext([isNext])`
+  lineChunked.isNext = getterSetter({
+    get: () => isNext,
+    set: (newValue) => { isNext = newValue; },
     setType: 'function',
     asConstant: (newValue) => () => !!newValue,
   });
